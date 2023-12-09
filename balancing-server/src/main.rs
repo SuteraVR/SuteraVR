151f2ced14c700568f1ebe612345e11c74f750b3
yank.nvim <yanknvim@gmail.com>
2023-12-09 15:42:31 +0900

    feat: Created balancing server

---
+++ balancing-server/src/main.rs
@@ -0,0 +1,41 @@
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::middleware::Logger;
+use serde::Serialize;
+
+use std::env;
+
+// INFO: struct Hello and handler hello are placeholder
+#[derive(Serialize)]
+struct Hello {
+    hello: String,
+}
+
+#[get("/hello")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().json(Hello {
+        hello: "world".to_string(),
+    })
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    env::set_var("RUST_LOG", "info");
+    env_logger::init();
+
+    // Check environment variable
+    let port = match env::var("PORT") {
+        Ok(val) => val.parse().unwrap(),
+        Err(_) => 3500,
+    };
+    log::info!("Run on port :{}", port);
+
+    // Run server
+    HttpServer::new(|| {
+        App::new()
+            .wrap(Logger::default())
+            .service(hello)
+    })
+    .bind(("127.0.0.1", port))?
+    .run()
+    .await
+}
