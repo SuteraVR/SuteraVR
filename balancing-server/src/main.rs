927a886f89f5df55e04381bae954e07d2ed9d061
yank.nvim <38945432+yanknvim@users.noreply.github.com>
2023-12-09 16:40:35 +0900

    fix: Fixed adress of server
    Co-authored-by: AsPulse / あすぱる <84216737+AsPulse@users.noreply.github.com>

--- balancing-server/src/main.rs
+++ balancing-server/src/main.rs
@@ -35,7 +35,7 @@ async fn main() -> std::io::Result<()> {
             .wrap(Logger::default())
             .service(hello)
     })
-    .bind(("127.0.0.1", port))?
+    .bind(("0.0.0.0", port))?
     .run()
     .await
 }
