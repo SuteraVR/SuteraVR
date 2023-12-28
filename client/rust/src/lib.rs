a7068d1d3df8c8b1645b6b477e84d92144173278
AsPulse <aspulse.x@gmail.com>
2023-12-28 21:59:49 +0900

    Remove old test

--- client/rust/src/lib.rs
+++ client/rust/src/lib.rs
@@ -12,14 +12,3 @@ struct MyExtension;
 
 #[gdextension]
 unsafe impl ExtensionLibrary for MyExtension {}
-
-#[cfg(test)]
-mod test {
-    use suteravr_lib::Foo;
-
-    /// whether this rust crate can load the suteravr-lib crate or not.
-    #[test]
-    fn test_loading_suteravrlib() {
-        let _foo: Foo = Foo::default();
-    }
-}
