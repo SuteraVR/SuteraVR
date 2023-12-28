95ae58afb713a28e35898e1bd24c3082f2fda26b
AsPulse <aspulse.x@gmail.com>
2023-12-28 13:44:43 +0900

    Add GetVersion schema

--- suteravr-lib/src/suterpc/mod.rs
+++ suteravr-lib/src/suterpc/mod.rs
@@ -5,27 +5,32 @@ use async_trait::async_trait;
 
 use self::oneshot_schema::OneshotVariants;
 
+// TODO: OneshotError type is need.
+type OneshotResult<Response> = Result<Response, ()>;
+
 #[async_trait]
-trait Server {
+trait Sender {
     async fn send_oneshot<'de, T: Oneshot<'de>>(
         &self,
         variant: OneshotVariants,
         payload: T::Request,
-    ) -> T::Response;
+    ) -> OneshotResult<T::Response>;
 }
 
 #[async_trait]
-trait Request<Response> {
-    async fn send(&self, server: &impl Server) -> Response;
+trait OneshotRequest<Response> {
+    async fn send<T: Sender + Send>(self, server: T) -> OneshotResult<Response>;
 }
 
 trait Oneshot<'de> {
-    type Request: Formula
+    type Request: Send
+        + Formula
         + BareFormula
         + SerializeRef<Self::Request>
         + Deserialize<'de, Self::Request>
-        + Request<Self::Response>;
-    type Response: Formula
+        + OneshotRequest<Self::Response>;
+    type Response: Send
+        + Formula
         + BareFormula
         + SerializeRef<Self::Response>
         + Deserialize<'de, Self::Response>;
--- suteravr-lib/src/suterpc/oneshot_schema.rs
+++ suteravr-lib/src/suterpc/oneshot_schema.rs
@@ -1,3 +1,30 @@
+use alkahest::alkahest;
+use async_trait::async_trait;
+
+use super::{Oneshot, OneshotRequest, OneshotResult, Sender};
+
 pub(crate) enum OneshotVariants {
     GetVersion = 1,
+}
+
+#[alkahest(Formula, SerializeRef, Deserialize)]
+pub(crate) struct GetVersionRequest {}
+#[async_trait]
+impl OneshotRequest<GetVersionResponse> for GetVersionRequest {
+    async fn send<T: Sender + Send>(self, server: T) -> OneshotResult<GetVersionResponse> {
+        server
+            .send_oneshot::<GetVersion>(OneshotVariants::GetVersion, self)
+            .await
+    }
+}
+
+#[alkahest(Formula, SerializeRef, Deserialize)]
+pub(crate) struct GetVersionResponse {
+    version: u64,
+}
+
+pub struct GetVersion {}
+impl Oneshot<'_> for GetVersion {
+    type Request = GetVersionRequest;
+    type Response = GetVersionResponse;
 }
