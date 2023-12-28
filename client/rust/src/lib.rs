00c9844604e6adc6ec4a3a5ee90e173ddac7eb50
AsPulse / あすぱる <84216737+AsPulse@users.noreply.github.com>
2023-12-28 23:00:36 +0900

    test(lib): more sweet testing about importing local crate (#77)

--- client/rust/src/lib.rs
+++ client/rust/src/lib.rs
@@ -1,17 +1,14 @@
+//! Client-side Rust code
+//!
+//! [`suteravr-lib`][suteravr_lib]が使用できます。
+//! ```no_run
+//! use suteravr_lib::Foo;
+//! ```
+//!
+
 use godot::prelude::*;
 
 struct MyExtension;
 
 #[gdextension]
 unsafe impl ExtensionLibrary for MyExtension {}
-
-#[cfg(test)]
-mod test {
-    use suteravr_lib::Foo;
-
-    /// whether this rust crate can load the suteravr-lib crate or not.
-    #[test]
-    fn test_loading_suteravrlib() {
-        let _foo: Foo = Foo::default();
-    }
-}
