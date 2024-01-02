9f846d5ad78e0f6369eeb8ed7becf21d3c3783ed
AsPulse <aspulse.x@gmail.com>
2024-01-02 11:19:45 +0900

    Fix gitignore is not honored due to cache

--- clocking-server/target/debug/deps/clocking_server-63ae9e7a12f29926.d
+++
@@ -1,5 +0,0 @@
-/home/legit/SuteraVR/SuteraVR/clocking-server/target/debug/deps/libclocking_server-63ae9e7a12f29926.rmeta: src/lib.rs
-
-/home/legit/SuteraVR/SuteraVR/clocking-server/target/debug/deps/clocking_server-63ae9e7a12f29926.d: src/lib.rs
-
-src/lib.rs:
