2b2b0d128f6c50939e5d6bd08ef8cc508770644c
yanknvim <yanknvim@gmail.com>
2024-01-25 21:52:50 +0900

    style: linted with clippy and cargo-fmt

--- balancing-server/src/bin/main.rs
+++ balancing-server/src/bin/main.rs
@@ -1,21 +1,16 @@
 use axum::{
-    routing::get,
-    response::{Response, Json},
-    Router,
-    middleware::{self, Next},
     extract::Request,
+    middleware::Next,
+    response::{Json, Response},
+    routing::get,
+    Router,
 };
-use tower::{
-    ServiceBuilder,
-};
+use tower::ServiceBuilder;
 
 use serde::Serialize;
 
+use http::StatusCode;
 use std::env;
-use http::{
-    StatusCode,
-    header::HeaderValue,
-};
 
 // INFO: struct Hello and handler hello are placeholder
 #[derive(Serialize)]
@@ -26,19 +21,23 @@ struct Hello {
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 async fn hello() -> Json<Hello> {
-    Json(Hello { hello: "world".to_string() })
+    Json(Hello {
+        hello: "world".to_string(),
+    })
 }
 
 async fn my_middleware(request: Request, next: Next) -> Result<Response, StatusCode> {
     match request.headers().get("SuteraVR-SchemaVersion") {
-        Some(version) if version == VERSION => {},
+        Some(version) if version == VERSION => {}
         _ => {
             return Err(StatusCode::BAD_REQUEST);
         }
     }
 
     let mut response = next.run(request).await;
-    response.headers_mut().insert("SuteraVR-SchemaVersion", VERSION.parse().unwrap());
+    response
+        .headers_mut()
+        .insert("SuteraVR-SchemaVersion", VERSION.parse().unwrap());
 
     Ok(response)
 }
@@ -54,15 +53,14 @@ async fn main() {
         Err(_) => 3500,
     };
     log::info!("Run on port :{}", port);
-    
+
     let app = Router::new()
         .route("/hello", get(hello))
-        .layer(
-            ServiceBuilder::new()
-                .layer(axum::middleware::from_fn(my_middleware))
-            );
+        .layer(ServiceBuilder::new().layer(axum::middleware::from_fn(my_middleware)));
 
-    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", port)).await.unwrap();
+    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", port))
+        .await
+        .unwrap();
 
     // Run server
     axum::serve(listener, app).await.unwrap();
