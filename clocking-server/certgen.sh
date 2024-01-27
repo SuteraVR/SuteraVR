e00adea350ae8f052ddde2471e585e8277133b06
AsPulse / あすぱる <contact@aspulse.dev>
2024-01-27 16:31:20 +0900

    Format certgen.sh

--- clocking-server/certgen.sh
+++ clocking-server/certgen.sh
@@ -1,3 +1,4 @@
 mkdir certs
 cd certs
-openssl req -x509 -newkey rsa:4096 -keyout server.key -out server.crt -days 365 -sha256 -nodes --subj '/CN=localhost/' && cd -
+openssl req -x509 -newkey rsa:4096 -keyout server.key -out server.crt -days 365 -sha256 -nodes --subj '/CN=localhost/'
+cd -
