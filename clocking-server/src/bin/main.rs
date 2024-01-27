1581df491e5b762e9bf3fac790e5da1d8b0e573d
AsPulse / あすぱる <contact@aspulse.dev>
2024-01-27 16:27:40 +0900

    Add echo server

--- clocking-server/src/bin/main.rs
+++ clocking-server/src/bin/main.rs
@@ -1,8 +1,12 @@
-use std::{io, path::PathBuf};
+use std::{io, net::SocketAddr, path::PathBuf, sync::Arc};
 
 use clocking_server::{certs::SingleCerts, consts};
 use log::{error, info};
-use tokio_rustls::rustls::ServerConfig;
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::TcpListener,
+};
+use tokio_rustls::{rustls::ServerConfig, TlsAcceptor};
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
@@ -29,7 +33,38 @@ async fn main() -> io::Result<()> {
     let cfg: ServerConfig = single_certs.gen_server_config()?;
 
     info!("");
-    info!("Server running on port :{}", *consts::PORT);
 
-    Ok(())
+    let addr = SocketAddr::from(([127, 0, 0, 1], *consts::PORT));
+
+    let acceptor = &TlsAcceptor::from(Arc::new(cfg));
+    let listener = TcpListener::bind(&addr).await?;
+
+    info!("Ready! Server running on {}", &addr);
+    loop {
+        let (strem, peer_addr) = listener.accept().await?;
+        let acceptor = acceptor.clone();
+        info!("Connection from {}...", peer_addr);
+
+        let fut = async move {
+            let mut stream = acceptor.accept(strem).await?;
+            info!("Connection from {} is established.", peer_addr);
+
+            let mut buf = vec![0; 1024];
+            while let Ok(n) = stream.read(&mut buf).await {
+                if n == 0 {
+                    break;
+                }
+                let string = String::from_utf8_lossy(&buf[..n]);
+                info!("Received from {}: {}", peer_addr, string.trim_end());
+                stream
+                    .write_all(format!("Received: {}", string).as_bytes())
+                    .await?;
+            }
+
+            info!("Connection from {} is closed.", peer_addr);
+            io::Result::Ok(())
+        };
+
+        tokio::spawn(fut);
+    }
 }
