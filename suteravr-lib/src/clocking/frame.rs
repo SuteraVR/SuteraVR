c3f88d7a41f794e1a15b49c0861b9f03d434fcab
AsPulse / あすぱる <contact@aspulse.dev>
2024-01-28 19:51:47 +0900

    Fix ClockingFrame should be checked remaining

--- suteravr-lib/src/clocking/frame.rs
+++ suteravr-lib/src/clocking/frame.rs
@@ -3,9 +3,30 @@ use std::{
     io::{self, BufWriter, Cursor, Write},
 };
 
+use bytes::Buf;
+
 /// Clocking-Server で扱うフレームを表すトレイトです。
-trait Framable: Sized + Send + Sync + Debug {
+pub trait ClockingFrame: Sized + Send + Sync + Debug {
+    type Context;
+    const MINIMAL_FRAME_SIZE: usize;
+
     /// フレームの解析を行います。
+    ///
+    /// フレームが解析可能であれば、`Some(Self)`を返し、そうでない場合は`None`を返します。
+    #[allow(async_fn_in_trait)]
+    async fn parse_frame(cursor: &mut Cursor<&[u8]>, ctx: Self::Context) -> Option<Self> {
+        if cursor.remaining() < Self::MINIMAL_FRAME_SIZE {
+            return None;
+        }
+        Self::parse_frame_unchecked(cursor, ctx).await
+    }
+
+    /// フレームの解析を行います。
+    ///
+    /// **このメソッドは直接呼びだすべきではありません。**  
+    /// 代わりに、`parse_frame`を呼び出してください。
+    ///
+    /// 以下は、`parse_frame_unchecked`を実装する際のための注釈です。
     ///
     /// ## 期待される動作
     /// 1. フレームの大きさが足りない場合は、直ちに `None` を返します。
@@ -14,8 +35,20 @@ trait Framable: Sized + Send + Sync + Debug {
     ///    このとき、`cursor` は、フレームの終端まで読み捨てている必要があります。
     /// 4. 解析に失敗した場合は、できるかぎり早期に`None` を返します。
     ///
-    async fn parse_frame(cursor: &mut Cursor<&[u8]>) -> Option<Self>;
+    /// この関数が呼び出された時点で、`cursor`は少なくとも `Self::MINIMAL_FRAME_SIZE` バイトのデータを保持していることが保証されるので、  
+    /// それに配慮する必要ありません。
+    #[allow(async_fn_in_trait)]
+    async fn parse_frame_unchecked(cursor: &mut Cursor<&[u8]>, ctx: Self::Context) -> Option<Self>;
 
     /// フレームの書き込みを行います。
-    async fn write_frame<W: ?Sized + Write>(&self, cursor: &mut BufWriter<W>) -> io::Result<()>;
+    #[allow(async_fn_in_trait)]
+    async fn write_frame<W: ?Sized + Write>(
+        &self,
+        cursor: &mut BufWriter<W>,
+        ctx: Self::Context,
+    ) -> io::Result<()>;
+}
+
+pub struct SuteraHeader {
+    pub message_length: u64,
 }
