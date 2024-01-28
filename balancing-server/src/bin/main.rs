a92eae28ed1a33473e7798c15f4b2ac731fa2c35
yanknvim <yanknvim@gmail.com>
2024-01-28 10:10:11 +0900

    feat(balancing): Create logging middleware

--- balancing-server/src/bin/main.rs
+++ balancing-server/src/bin/main.rs
@@ -48,6 +48,18 @@ async fn schemaversion_checker(request: Request, next: Next) -> Result<Response,
     Ok(response)
 }
 
+async fn logger(request: Request, next: Next) -> Response {
+    let log_str = format!(
+        "{} {} {:?}",
+        request.method(),
+        request.uri(),
+        request.version()
+    );
+    let response = next.run(request).await;
+    log::info!("{} {}", log_str, response.status().as_str());
+    response
+}
+
 #[tokio::main]
 async fn main() {
     env::set_var("RUST_LOG", "info");
@@ -60,9 +72,11 @@ async fn main() {
     };
     log::info!("Run on port :{}", port);
 
-    let app = Router::new()
-        .route("/hello", get(hello))
-        .layer(ServiceBuilder::new().layer(axum::middleware::from_fn(schemaversion_checker)));
+    let app = Router::new().route("/hello", get(hello)).layer(
+        ServiceBuilder::new()
+            .layer(axum::middleware::from_fn(logger))
+            .layer(axum::middleware::from_fn(schemaversion_checker)),
+    );
 
     let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", port))
         .await
