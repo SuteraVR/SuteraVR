3964bc9abb52f7a30e793ba740364beb95b53df8
See2et <see2et@see2et.dev>
2024-01-31 19:25:43 +0900

    jump!!!

--- client/Godot/scenes/Player.gd
+++ client/Godot/scenes/Player.gd
@@ -14,20 +14,15 @@ var mouse_raw: Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	CAMERA_CONTROLLER = $XROrigin3D/XRCamera3D
-	
-func _input(event):
-	if Input.is_action_just_pressed("release_cursor"):
-		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-	if Input.is_action_just_pressed("left_click"):
-		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
 
 func _unhandled_input(event):
 	mouse_input = event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED
 	if mouse_input:
 		mouse_raw = Vector2(-event.relative.y, -event.relative.x)
-	
+
+
 func _update_camera(delta):
 	mouse_rotation += mouse_raw * delta * SENSITIVITY
 	mouse_rotation.x = clamp(mouse_rotation.x, TILT_LOWER_LIMIT, TILT_UPPER_LIMIT)
@@ -40,15 +35,6 @@ func _update_camera(delta):
 	
 	mouse_raw = Vector2.ZERO
 
+
 func _physics_process(delta):
 	_update_camera(delta)
-	
-	var direction = Vector2(
-		int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left")),
-		int(Input.is_action_pressed("move_back")) - int(Input.is_action_pressed("move_forward"))
-	)
-	
-	if direction != Vector2.ZERO:
-		var looked = direction.rotated(-mouse_rotation.y).normalized() * SPEED
-		velocity = Vector3(looked.x, 0, looked.y)
-		move_and_slide()
