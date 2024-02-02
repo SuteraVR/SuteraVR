0da40bac8c905aa6172b645cd3ed635a152bf371
AsPulse / あすぱる <contact@aspulse.dev>
2024-02-02 22:25:49 +0900

    Adding player-control

--- clocking-server/src/instance/mod.rs
+++ clocking-server/src/instance/mod.rs
@@ -1,4 +1,7 @@
+use std::collections::{hash_map::Entry, HashMap};
+
 use derivative::Derivative;
+use log::warn;
 use suteravr_lib::{
     clocking::schemas::oneshot::chat_entry::ChatEntry,
     info,
@@ -12,9 +15,12 @@ use crate::{errors::InstanceError, shutdown::ShutdownReason};
 pub mod manager;
 pub enum InstanceControl {
     Shutdown(ShutdownReason),
-    Join(PlayerId),
+    Join(PlayerId, mpsc::Sender<PlayerControl>),
     Leave(PlayerId),
     ChatMesasge(ChatEntry),
+}
+pub enum PlayerControl {
+    NewChatMessage(ChatEntry),
 }
 
 #[derive(Derivative)]
@@ -22,7 +28,7 @@ pub enum InstanceControl {
 pub struct Instance {
     pub id: InstanceId,
     pub world: WorldId,
-    pub players: Vec<PlayerId>,
+    pub players: HashMap<PlayerId, mpsc::Sender<PlayerControl>>,
     pub chat_history: Vec<ChatEntry>,
 
     #[derivative(Debug = "ignore")]
@@ -33,7 +39,7 @@ impl Instance {
     fn new(
         id: InstanceId,
         world: WorldId,
-        players: Vec<PlayerId>,
+        players: HashMap<PlayerId, mpsc::Sender<PlayerControl>>,
         chat_history: Vec<ChatEntry>,
         logger: EnvLogger,
     ) -> Self {
@@ -55,7 +61,7 @@ pub async fn launch_instance(
     let logger = EnvLogger {
         target: format!("instance-{:?}", id),
     };
-    let mut instance = Instance::new(id, world, Vec::new(), Vec::new(), logger.clone());
+    let mut instance = Instance::new(id, world, HashMap::new(), Vec::new(), logger.clone());
     info!(logger, "Instance started.");
 
     loop {
@@ -65,12 +71,19 @@ pub async fn launch_instance(
                     InstanceControl::Shutdown(_) => {
                         break;
                     },
-                    InstanceControl::Join(player_id) => {
-                        instance.players.push(player_id);
-                        info!(logger, "Player joined (id: {:?}), currently {} player(s) in instance.", player_id, instance.players.len());
+                    InstanceControl::Join(player_id, sender) => {
+                        match instance.players.entry(player_id) {
+                            Entry::Vacant(o) => {
+                                o.insert(sender);
+                                info!(logger, "Player joined (id: {:?}), currently {} player(s) in instance.", player_id, instance.players.len());
+                            },
+                            Entry::Occupied(o) => {
+                                o.insert(sender);
+                                warn!(logger, "Join request received but already in instance (id: {:?}).", player_id);
+                            }
+                        }
                     },
                     InstanceControl::Leave(player_id) => {
-                        instance.players.retain(|&id| id != player_id);
                         info!(logger, "Player left: (id: {:?}), currently {} player(s) in instance.", player_id, instance.players.len());
                     },
                     InstanceControl::ChatMesasge(chat_entry) => {
