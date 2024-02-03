710aad2f834ad8a9eb95b29deb142469cbe6cdcc
AsPulse / あすぱる <contact@aspulse.dev>
2024-02-03 20:07:26 +0900

    Add looking_around to implement

--- client/Godot/scenes/looking_around.gd
+++ client/Godot/scenes/looking_around.gd
@@ -10,16 +10,17 @@ const TILT_LOWER_LIMIT := deg_to_rad(-90.0)
 const TILT_UPPER_LIMIT := deg_to_rad(90.0)
 const SENSITIVITY = 0.3
 
-
-var CAMERA_CONTROLLER: XRCamera3D
-@export var order = 9
-
 var mouse_input = false
 var mouse_rotation: Vector2
 var mouse_raw: Vector2
 
+@onready var origin_node : XROrigin3D = XRHelpers.get_xr_origin(self)
+@onready var camera_node : XRCamera3D = XRHelpers.get_xr_camera(self)
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	var origin = get_parent().get_parent()
 	pass
 
 
@@ -29,9 +30,14 @@ func _unhandled_input(event):
 		mouse_raw = Vector2(-event.relative.y, -event.relative.x)
 
 
-func physics_movement(delta: float, player_body: XRToolsPlayerBody, _disabled: bool):
+func _update_camera(delta):
 	mouse_rotation += mouse_raw * delta * SENSITIVITY
-	# mouse_rotation.x = clamp(mouse_rotation.x, TILT_LOWER_LIMIT, TILT_UPPER_LIMIT)
+	mouse_rotation.x = clamp(mouse_rotation.x, TILT_LOWER_LIMIT, TILT_UPPER_LIMIT)
 	mouse_rotation.y = fmod(mouse_rotation.y, 2*PI)
-	
-	# player_body.rotate_player(delta * mouse_rotation.y)
+	camera_node.rotation.x = mouse_rotation.x
+	origin_node.rotation.y = mouse_rotation.y
+	mouse_raw = Vector2.ZERO
+
+
+func _physics_process(delta):
+	_update_camera(delta)
