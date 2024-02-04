845e370b839c587261380100f8724e7551b40a41
AsPulse / あすぱる <contact@aspulse.dev>
2024-02-04 15:55:41 +0900

    feat(client/rust): connect to the clocking server by SRV Record, unsafe localhost (#127)

--- client/rust/src/tcp/mod.rs
+++ client/rust/src/tcp/mod.rs
@@ -1,19 +1,15 @@
 pub mod allow_unknown_cert;
+pub mod conenction;
 pub mod error;
 pub mod requests;
 
 use alkahest::deserialize;
-use std::{
-    collections::{hash_map::Entry, HashMap},
-    sync::{atomic::AtomicU64, Arc},
-};
+use hickory_resolver::TokioAsyncResolver;
+use std::sync::{atomic::AtomicU64, Arc, Mutex};
 use suteravr_lib::{
-    clocking::{
-        event_headers::EventTypes,
-        schemas::oneshot::{
-            chat_entry::{SendChatMessageRequest, SendableChatEntry},
-            login::{LoginRequest, LoginResponse},
-        },
+    clocking::schemas::oneshot::{
+        chat_entry::SendChatMessageRequest,
+        login::{LoginRequest, LoginResponse},
     },
     error,
     util::serialize_to_new_vec,
@@ -23,41 +19,37 @@ use futures::executor::block_on;
 use godot::{engine::notify::NodeNotification, obj::WithBaseField, prelude::*};
 use suteravr_lib::{
     clocking::{
-        buffer::{ContentHeader, FrameBuffer},
         oneshot_headers::{OneshotHeader, OneshotStep, OneshotTypes},
         sutera_header::SuteraHeader,
-        traits::MessageAuthor,
-        ClockingConnection, ClockingFrameUnit,
     },
     info,
     messaging::id::MessageId,
     warn, SCHEMA_VERSION,
 };
 use tokio::{
-    net::TcpStream,
     sync::{mpsc, oneshot},
-    task::{JoinError, JoinHandle},
+    task::JoinError,
 };
-use tokio_rustls::{
-    rustls::{pki_types::ServerName, ClientConfig},
-    TlsConnector,
-};
+use tokio_rustls::rustls::ClientConfig;
 
 use crate::{
     async_driver::tokio,
     logger::GodotLogger,
-    signal_names::SIGNAL_NEW_TEXTCHAT_MESSAGE,
+    signal_names::{SIGNAL_CONNECTION_ESTABLISHED, SIGNAL_NEW_TEXTCHAT_MESSAGE},
     tcp::{
         allow_unknown_cert::AllowUnknownCertVerifier,
         error::TcpServerError,
-        requests::{EventMessage, OneshotRequest, OneshotResponse},
+        requests::{OneshotRequest, OneshotResponse},
     },
 };
 
-use self::requests::{Request, Response};
+use self::{
+    conenction::Connection,
+    requests::{Request, Response},
+};
 
 #[derive(Debug)]
-enum ShutdownReason {
+pub enum ShutdownReason {
     GameExit,
 }
 
@@ -66,10 +58,7 @@ enum ShutdownReason {
 struct ClockerConnection {
     base: Base<Node>,
     logger: GodotLogger,
-    shutdown_tx: Option<oneshot::Sender<ShutdownReason>>,
-    receive_rx: Option<mpsc::Receiver<Request>>,
-    send_tx: Option<mpsc::Sender<Response>>,
-    handle: Option<JoinHandle<()>>,
+    connection: Arc<Mutex<Option<Connection>>>,
     message_id_dispatch: AtomicU64,
 }
 
@@ -79,11 +68,13 @@ impl ClockerConnection {
     }
 
     async fn shutdown(&mut self) -> Result<(), JoinError> {
-        if let Some(tx) = self.shutdown_tx.take() {
-            tx.send(ShutdownReason::GameExit).unwrap();
-        }
-        if let Some(handle) = self.handle.take() {
-            handle.await?;
+        let taken_connection = { self.connection.lock().unwrap().take() };
+        if let Some(connection) = taken_connection {
+            connection
+                .shutdown_tx
+                .send(ShutdownReason::GameExit)
+                .unwrap();
+            connection.handle.await?;
         }
         Ok(())
     }
@@ -95,164 +86,92 @@ impl ClockerConnection {
     fn signal_new_textchat_message(&mut self) -> String {
         SIGNAL_NEW_TEXTCHAT_MESSAGE.to_string()
     }
+    #[func]
+    fn signal_connection_established(&mut self) -> String {
+        SIGNAL_CONNECTION_ESTABLISHED.to_string()
+    }
 
     #[func]
-    fn connect_sutera_clocking_without_certverify(&mut self, name: String, addr: String) {
-        info!(self.logger, "Making connection...");
-
-        let (shutdown_tx, mut shutdown_rx) = oneshot::channel::<ShutdownReason>();
-        self.shutdown_tx = Some(shutdown_tx);
-
-        let (receive_tx, receive_rx) = mpsc::channel::<Request>(32);
-        self.receive_rx = Some(receive_rx);
-
-        let (send_tx, mut send_rx) = mpsc::channel::<Response>(32);
-        self.send_tx = Some(send_tx.clone());
-
+    fn connect_by_srv(&mut self, domain: String) {
+        let instance_id = self.base().instance_id();
         let logger = self.logger();
-
-        // FIXME: it is panicable
-        let id = self.base().instance_id();
-        let gd = Gd::<ClockerConnection>::from_instance_id(id).cast::<ClockerConnection>();
-        let id = gd.instance_id();
-
-        let server = async move {
-            let mut reply_senders = HashMap::<MessageId, oneshot::Sender<Request>>::new();
-
-            info!(logger, "Connecting to {}({}) ...", name, addr);
-
-            let config = ClientConfig::builder()
-                .dangerous()
-                .with_custom_certificate_verifier(AllowUnknownCertVerifier::new())
-                .with_no_client_auth();
-            warn!(logger, "Allowing unknown certificates.");
-            warn!(logger, "Ensure you are connecting to the right server!");
-
-            let connector = TlsConnector::from(Arc::new(config));
-            let dnsname = ServerName::try_from(name).unwrap();
-
-            let stream = TcpStream::connect(&addr)
-                .await
-                .map_err(TcpServerError::ConnectingError)?;
-
-            let stream = connector
-                .connect(dnsname, stream)
-                .await
-                .map_err(TcpServerError::ConnectingError)?;
-
-            info!(logger, "Connection established!");
-
-            let mut connection = ClockingConnection::new(stream, MessageAuthor::Server);
-            let mut frame_buffer = FrameBuffer::new(logger.clone());
-
-            let receive = receive_tx;
-            let _reply = send_tx.clone();
-
-            loop {
-                tokio::select! {
-                    Some(request) = send_rx.recv() => {
-                        match request {
-                            Response::Oneshot(oneshot) => {
-                                connection.write_frame(&ClockingFrameUnit::SuteraHeader(oneshot.sutera_header)).await?;
-                                connection.write_frame(&ClockingFrameUnit::OneshotHeaders(oneshot.oneshot_header)).await?;
-                                connection.write_frame(&ClockingFrameUnit::Content(oneshot.payload)).await?;
-                            },
-                            Response::OneshotWithReply(oneshot, sender) => {
-                                let Entry::Vacant(o) = reply_senders.entry(oneshot.oneshot_header.message_id) else {
-                                    error!(logger, "MessageId {:?} is already occupied!", oneshot.oneshot_header.message_id);
-                                    panic!();
-                                };
-                                o.insert(sender);
-                                connection.write_frame(&ClockingFrameUnit::SuteraHeader(oneshot.sutera_header)).await?;
-                                connection.write_frame(&ClockingFrameUnit::OneshotHeaders(oneshot.oneshot_header)).await?;
-                                connection.write_frame(&ClockingFrameUnit::Content(oneshot.payload)).await?;
-                            }
-                        }
-                    },
-                    read = connection.read_frame() => {
-                        match read {
-                            Ok(Some(payload)) => {
-                                if let Some(received) = frame_buffer.append(payload, MessageAuthor::Server) {
-                                    if received.sutera_status.is_none() {
-                                        panic!("Received message doesn't contain sutera_header! (Maybe frame_buffer has bugs.)")
-                                    }
-                                    match received.content_header {
-                                        ContentHeader::Event(event_header) if event_header.message_type == EventTypes::TextChat_ReceiveChatMessage_Push => {
-                                            let chat_message = deserialize::<SendableChatEntry, SendableChatEntry>(&received.payload)?; Gd::<ClockerConnection>::from_instance_id(id).cast::<ClockerConnection>().call_deferred(
-                                                "emit_signal".into(),
-                                                &[
-                                                    Variant::from(SIGNAL_NEW_TEXTCHAT_MESSAGE.into_godot()),
-                                                    Variant::from(chat_message.sender.into_godot()),
-                                                    Variant::from(chat_message.message.into_godot()),
-                                                ] ,
-                                            );
-
-                                        }
-                                        ContentHeader::Event(event_header) => {
-                                            receive.send(
-                                                Request::Event(EventMessage::new(
-                                                    received.sutera_header,
-                                                    event_header,
-                                                    received.payload,
-                                                ))
-                                            ).await.map_err(TcpServerError::CannotSendRequest)?;
-                                        }
-                                        ContentHeader::Oneshot(oneshot_header) => {
-                                            // FIXME: Push型かPull型かチェックしてあげないといけない
-                                            if let Entry::Occupied(o) = reply_senders.entry(oneshot_header.message_id) {
-                                                o.remove_entry().1.send(Request::Oneshot(OneshotRequest::new(
-                                                    received.sutera_header,
-                                                    received.sutera_status.unwrap(),
-                                                    oneshot_header,
-                                                    received.payload,
-                                                ))).map_err(|_| TcpServerError::CannotSendOneshotReply)?;
-                                            } else {
-                                                receive.send(
-                                                    Request::Oneshot(OneshotRequest::new(
-                                                        received.sutera_header,
-                                                        received.sutera_status.unwrap(),
-                                                        oneshot_header,
-                                                        received.payload,
-                                                    ))
-                                                ).await.map_err(TcpServerError::CannotSendRequest)?;
-                                            }
-                                        },
-                                    }
-                                }
-                            }
-                            Ok(None) => {
-                                break;
-                            }
-                            Err(e)=> {
-                                warn!(logger, "{}", e);
-                                break;
-                            }
-                        }
-                    },
-                    _shutdown = &mut shutdown_rx => {
-                        break;
+        let connection = self.connection.clone();
+        tokio().bind().spawn("connect_by_srv", async move {
+            let resolver = TokioAsyncResolver::tokio_from_system_conf().unwrap();
+            let srv = resolver
+                .srv_lookup(format!("_suteravr-clocker._tls.{}", domain))
+                .await;
+            match srv.map(|v| v.into_iter().next()) {
+                Ok(Some(e)) => {
+                    info!(logger, "SRV record resolved: {:?}", e);
+                    let mut root_cert_store = tokio_rustls::rustls::RootCertStore::empty();
+                    for cert in rustls_native_certs::load_native_certs().unwrap() {
+                        root_cert_store.add(cert).unwrap();
                     }
+                    let config = ClientConfig::builder()
+                        .with_root_certificates(root_cert_store)
+                        .with_no_client_auth();
+                    Self::connect(
+                        connection,
+                        logger,
+                        instance_id,
+                        config,
+                        domain,
+                        format!("{}:{}", e.target(), e.port()),
+                    )
+                }
+                Ok(None) => {
+                    error!(
+                        logger,
+                        "Failed to resolve SRV record: SRV record not found."
+                    );
+                }
+                Err(e) => {
+                    error!(logger, "Failed to resolve SRV record: {:?}", e);
                 }
             }
+        });
+    }
 
-            connection.shutdown_stream().await?;
-            Ok::<(), TcpServerError>(())
-        };
+    #[func]
+    fn connect_to_localhost(&mut self) {
+        self.connect_to_localhost_with_port(3501)
+    }
 
-        let logger = self.logger();
-        self.handle = Some(tokio().bind().spawn("clocker_connection", async move {
-            match server.await {
-                Ok(_) => info!(logger, "Connection successfully finished."),
-                Err(e) => warn!(logger, "Connection failed: {}", e),
-            }
-        }));
+    #[func]
+    fn connect_to_localhost_with_port(&mut self, port: u16) {
+        self.connect_without_certification_verifying(
+            "localhost".into(),
+            format!("127.0.0.1:{}", port),
+        )
+    }
+
+    #[func]
+    fn connect_without_certification_verifying(&mut self, name: String, addr: String) {
+        let config = ClientConfig::builder()
+            .dangerous()
+            .with_custom_certificate_verifier(AllowUnknownCertVerifier::new())
+            .with_no_client_auth();
+        warn!(self.logger, "Allowing unknown certificates.");
+        warn!(
+            self.logger,
+            "Ensure you are connecting to the right server!"
+        );
+        Self::connect(
+            self.connection.clone(),
+            self.logger.clone(),
+            self.base().instance_id(),
+            config,
+            name,
+            addr,
+        );
     }
 
     #[func]
     fn oneshot_send_chat_message(&mut self, content: String) {
         let id = self.get_message_id();
         let logger = self.logger();
-        let send = self.send_tx.clone().unwrap();
+        let send = self.send_tx();
         tokio().bind().spawn("clocking_request", async move {
             let login_result = Self::create_oneshot_p(
                 logger.clone(),
@@ -281,7 +200,7 @@ impl ClockerConnection {
     fn join_instance(&mut self, join_token: u64) {
         let id = self.get_message_id();
         let logger = self.logger();
-        let send = self.send_tx.clone().unwrap();
+        let send = self.send_tx();
         tokio().bind().spawn("clocking_request", async move {
             info!(logger, "Joining instance with token: {}", join_token);
             let login_result = Self::create_oneshot_p(
@@ -308,6 +227,29 @@ impl ClockerConnection {
     }
 }
 impl ClockerConnection {
+    fn send_tx(&self) -> mpsc::Sender<Response> {
+        return self
+            .connection
+            .lock()
+            .unwrap()
+            .as_ref()
+            .unwrap()
+            .send_tx
+            .clone();
+    }
+
+    fn connect<T: Into<String> + Send + 'static>(
+        connection: Arc<Mutex<Option<Connection>>>,
+        logger: GodotLogger,
+        instance_id: InstanceId,
+        config: ClientConfig,
+        name: T,
+        addr: T,
+    ) {
+        *connection.lock().unwrap() =
+            Some(Connection::new(logger, instance_id, config, name, addr));
+    }
+
     async fn create_oneshot_p(
         logger: GodotLogger,
         send: mpsc::Sender<Response>,
@@ -346,10 +288,7 @@ impl INode for ClockerConnection {
         Self {
             base,
             logger,
-            handle: None,
-            shutdown_tx: None,
-            receive_rx: None,
-            send_tx: None,
+            connection: Arc::new(Mutex::new(None)),
             message_id_dispatch: AtomicU64::new(0),
         }
     }
@@ -357,6 +296,8 @@ impl INode for ClockerConnection {
     fn ready(&mut self) {
         self.base_mut()
             .add_user_signal(SIGNAL_NEW_TEXTCHAT_MESSAGE.into());
+        self.base_mut()
+            .add_user_signal(SIGNAL_CONNECTION_ESTABLISHED.into());
     }
 
     fn on_notification(&mut self, what: NodeNotification) {
