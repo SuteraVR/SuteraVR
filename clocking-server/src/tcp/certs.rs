683f1189a800905f5d4e0696c6412f9ad711c407
yank.nvim <38945432+yanknvim@users.noreply.github.com>
2024-02-15 22:56:44 +0900

    feat(clocking): loading certifications from env (#140)
    Co-authored-by: AsPulse / あすぱる <contact@aspulse.dev>

--- clocking-server/src/tcp/certs.rs
+++ clocking-server/src/tcp/certs.rs
@@ -1,8 +1,10 @@
+use log::info;
 use rustls_pemfile::{certs, pkcs8_private_keys};
 use std::{
+    env,
     fs::File,
-    io::{self, BufReader},
-    path::Path,
+    io::{self, BufReader, Error, ErrorKind},
+    path::PathBuf,
 };
 use tokio_rustls::rustls::{
     self,
@@ -15,16 +17,78 @@ pub struct SingleCerts {
     pub keys: PrivateKeyDer<'static>,
 }
 
-impl SingleCerts {
-    pub fn new(certs_path: &Path, keys_path: &Path) -> io::Result<Self> {
-        Ok(Self {
-            certs: certs(&mut BufReader::new(File::open(certs_path)?))
+fn load_from_env() -> io::Result<Option<SingleCerts>> {
+    let Ok(certs_env) = env::var("SINGLECERTS_CERT_PEM") else {
+        return Ok(None);
+    };
+    let Ok(keys_env) = env::var("SINGLECERTS_KEY_PEM") else {
+        return Ok(None);
+    };
+
+    let keys = pkcs8_private_keys(&mut keys_env.as_bytes())
+        .next()
+        .ok_or(io::ErrorKind::InvalidInput)?
+        .map(Into::into)?;
+    Ok(Some(SingleCerts {
+        certs: certs(&mut certs_env.as_bytes())
+            .collect::<io::Result<Vec<CertificateDer<'static>>>>()?,
+        keys,
+    }))
+}
+
+fn load_from_env_path() -> io::Result<Option<(SingleCerts, String, String)>> {
+    let Ok(certs_path) = env::var("SINGLECERTS_CERT_PATH") else {
+        return Ok(None);
+    };
+    let Ok(keys_path) = env::var("SINGLECERTS_KEY_PATH") else {
+        return Ok(None);
+    };
+    let certs_file = &PathBuf::from(&certs_path);
+    let keys_file = &PathBuf::from(&keys_path);
+    Ok(Some((
+        SingleCerts {
+            certs: certs(&mut BufReader::new(File::open(certs_file)?))
                 .collect::<io::Result<Vec<CertificateDer<'static>>>>()?,
-            keys: pkcs8_private_keys(&mut BufReader::new(File::open(keys_path)?))
+            keys: pkcs8_private_keys(&mut BufReader::new(File::open(keys_file)?))
                 .next()
                 .ok_or(io::ErrorKind::InvalidInput)?
                 .map(Into::into)?,
-        })
+        },
+        certs_path,
+        keys_path,
+    )))
+}
+
+fn load_from_file() -> io::Result<Option<SingleCerts>> {
+    let certs_path = &PathBuf::from("./certs/server.crt");
+    let keys_path = &PathBuf::from("./certs/server.key");
+    Ok(Some(SingleCerts {
+        certs: certs(&mut BufReader::new(File::open(certs_path)?))
+            .collect::<io::Result<Vec<CertificateDer<'static>>>>()?,
+        keys: pkcs8_private_keys(&mut BufReader::new(File::open(keys_path)?))
+            .next()
+            .ok_or(io::ErrorKind::InvalidInput)?
+            .map(Into::into)?,
+    }))
+}
+
+impl SingleCerts {
+    pub fn new() -> io::Result<Self> {
+        if let Some(certs) = load_from_env()? {
+            info!("Certifications loaded from environment variables.");
+            return Ok(certs);
+        }
+        if let Some((certs, certs_path, keys_path)) = load_from_env_path()? {
+            info!("Certification loaded from {:?}", certs_path);
+            info!("Private key loaded from {:?}", keys_path);
+            return Ok(certs);
+        }
+        if let Some(certs) = load_from_file()? {
+            info!("Certification loaded from ./certs/server.crt");
+            info!("Private key loaded from ./certs/server.key");
+            return Ok(certs);
+        }
+        Err(Error::from(ErrorKind::NotFound))
     }
     pub fn gen_server_config(self) -> io::Result<ServerConfig> {
         rustls::ServerConfig::builder_with_protocol_versions(&[&rustls::version::TLS13])
