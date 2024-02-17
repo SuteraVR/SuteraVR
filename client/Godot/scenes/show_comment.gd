dcc8586a4f611c2ebde92f2eeb60539df617c6d3
LegitYew0108 <legitlegit0108@gmail.com>
2024-02-17 09:23:29 +0900

    feat: made teleport func but teleport permanently

--- client/Godot/scenes/show_comment.gd
+++ client/Godot/scenes/show_comment.gd
@@ -2,6 +2,7 @@ extends Label
 
 @export var line_edit:Control
 @onready var instance_anchor:Node = %InstanceAnchor
+@onready var player_body:CharacterBody3D = %CharacterBody3D
 
 func _process(delta):
 	var comment = line_edit.message_box
@@ -11,10 +12,18 @@ func _process(delta):
 			if(comment_array[0]=="//ch_av"):
 				instance_anchor.change_user_avatar(line_edit.name_box,comment_array[1].to_int())
 				text = "changed avatar number %s" % comment_array[1]
+			elif(comment_array[0] == "//ch_world"):
+				teleport_world(comment_array[1].to_int())
 			else:
 				text = "invalid command"
 		else:
 			text = "%s" % comment
 	else:
 		text = "%s" % comment
-	comment.my_comment = false
+	line_edit.my_comment = false
+
+func teleport_world(world_num:int):
+	const world1 = Vector3(3,0,0)
+	if(world_num==1):
+		player_body.accept_teleport(world1)
+		print("teleporting")
