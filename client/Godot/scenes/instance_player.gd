b962d9821182c918f71a2c83e2588082c5bc03d0
AsPulse / あすぱる <contact@aspulse.dev>
2024-02-17 13:21:38 +0900

    commit once

--- client/Godot/scenes/instance_player.gd
+++ client/Godot/scenes/instance_player.gd
@@ -4,12 +4,18 @@ class_name PlayerInstance
 var Scene: Node3D
 var PlayerId: int
 var Clocker: ClockerConnection
+var delay: Array[int] = [];
 
 const player_scene = preload("res://scenes/instance_player.tscn")
 const player_scene1 = preload("res://tsukurun-world/avatars/ash/ash_1_0.tscn")
 const player_scene2 = preload("res://tsukurun-world/avatars/ciel/ciel_1_0.tscn")
 const player_scene3 = preload("res://scenes/3dmodels/Shapell.tscn")
 
+func reset_delay():
+	delay.clear()
+	var start = Time.get_ticks_msec()
+	for n in 10:
+		delay.push_back(start + (n-10) * 16)
 
 func _init(clocker: ClockerConnection, player_id: int):
 	self.Clocker = clocker
@@ -18,6 +24,8 @@ func _init(clocker: ClockerConnection, player_id: int):
 	self.Scene.visible = false
 	add_child(self.Scene)
 	print("Player %s initialized." % [PlayerId])
+	reset_delay()
+
 
 func appear():
 	self.Scene.visible = true
@@ -37,10 +45,21 @@ func change_avatar(avatar_num:int):
 	add_child(self.Scene)
 	self.Scene.visible = true
 
+func calc_delay() -> int:
+	var s = delay.pop_front()
+	var e = Time.get_ticks_msec()
+	if e - s > 1000:
+		reset_delay()
+		return 16
+	return 0
+	
+
 func move(
 	x: float, y: float, z: float,
 	xx: float, xz: float, zx: float, zz: float,
 ):
+
+	print()
 	self.Scene.transform.origin = Vector3(x, y, z)
 	self.Scene.transform.basis.x.x = xx
 	self.Scene.transform.basis.x.z = xz
